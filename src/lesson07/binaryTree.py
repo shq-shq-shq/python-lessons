fac96b0c95c676e8256329ac269ad4530f245d84
Kirill Shirokov <kirill.shirokov@gmail.com>
2019-07-21 01:00:06 -0700

    binary tree example and logging

--- src/lesson07/binaryTree.py
+++ src/lesson07/binaryTree.py
@@ -1,3 +1,4 @@
+import random
 
 class Node:
     def __init__(self, value, left=None, right=None):
@@ -7,17 +8,22 @@ class Node:
 
     def searchRecursive(self, value):
         if self.value == value:
+            print("    NODE: !%s!" % self.value)
             return (True, self)
         elif value < self.value:
             if self.left is None:
+                print("    NODE: None<-%s" % self.value)
                 return (False, self)
             else:
-                return self.left.search(value)
+                print("    NODE: <-%s" % self.value)
+                return self.left.searchRecursive(value)
         else:
             if self.right is None:
+                print("    NODE: %s->None" % self.value)
                 return (False, self)
             else:
-                return self.right.search(value)
+                print("    NODE: %s->" % self.value)
+                return self.right.searchRecursive(value)
 
     def contains(self, value):
         found, node = self.searchRecursive(value)
@@ -41,13 +47,30 @@ class BinaryTree:
         self.root = None
 
     def add(self, value):
+        print("tree: add %s" % value)
         if self.root is None:
             self.root = Node(value)
         else:
             self.root.add(value)
 
+    def addAll(self, values):
+        for v in values:
+            self.add(v)
+
     def contains(self, value):
+        print("tree: search %s" % value)
         if self.root is None:
             return False
         else:
             return self.root.contains(value)
+
+
+tree = BinaryTree()
+
+numbers = list(range(55)) + list(range(56, 101, 2))
+random.shuffle(numbers)
+tree.addAll(numbers)
+
+print(tree.contains(10))
+print(tree.contains(55))
+print(tree.contains(1000))
